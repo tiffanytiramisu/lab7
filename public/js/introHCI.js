e140d9df0089289b69cdbfa394b4c7ccd053c2e6
Tiffany Su <ttsu@ucsd.edu>
2018-02-22 02:15:07 -0800

    viewAlt route

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,11 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+	$(".likeBtn").click(function(){
+		ga("send", "event", 'like', 'click');
+	});
+	$(".btn.btn-default.likeBtn").click(function(){
+		ga("send", "event", 'like', 'click');
+	});
 	// your code here
 }
